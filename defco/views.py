09cc95df11bf4bdda53e95693df90c762da0b1f9
Ken Thumi <kenthumi@gmail.com>
2022-01-12 14:13:46 +0300

    Logout added

--- defco/views.py
+++ defco/views.py
@@ -1,8 +1,9 @@
 from defco.forms import UserRegisterForm
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required
 def home(request):
     ctx = {'lorem':'lorem'}
     return render(request,'index.html',ctx)
